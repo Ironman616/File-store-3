4b63482308e82bfce44fdff7634d21963d08dc21
Ironman616 <150930956+Ironman616@users.noreply.github.com>
2024-09-07 18:13:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -60,7 +60,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌ Don't send me messages directly I'm only File Share bot!!\n\n» ᴍʏ ᴏᴡɴᴇʀ : @mladminbot"
+USER_REPLY_TEXT = "❌ Don't send me messages directly I'm only File Share bot!!\n\n Channel lo search Cheyi bro or Use our Movies search bot 😉»"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(6497757690)
